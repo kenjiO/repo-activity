785726a0a5c085b8b184259b4a16de3c55530638
Kenji Okamoto <kenjio@users.noreply.github.com>
2018-02-05 16:29:08 -0800

    project setup

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const foo = () => 'bar';
+
+export default foo;
+console.info('This is repo-activity');
---
+++ src/index.test.js
@@ -0,0 +1,6 @@
+import { expect } from 'chai';
+import foo from './index';
+
+it('', () => {
+  expect(foo()).to.eql('bar');
+});
